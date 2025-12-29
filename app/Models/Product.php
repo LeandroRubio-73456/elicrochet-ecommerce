b7d2ce1ff83546379d7840add24ba6cc2ee8496c
Leandro Rubio <leandrorubio456@hotmail.com>
2025-12-29 18:40:08 -0500

    feat: Create `Product` model with fillable attributes, casts, accessors, scopes, and relationships.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,11 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
 class Product extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'name',
         'slug',
